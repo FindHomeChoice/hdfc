de31ff39ed0f9e73e2f9195ed19bae6889204c88
FindHomeChoice <finidinghome@gmail.com>
2025-04-18 15:34:15 +0530

    Update SuccessPage.jsx

--- src/SuccessPage.jsx
+++ src/SuccessPage.jsx
@@ -13,7 +13,7 @@ const SuccessPage = ({ forwardingNumber }) => {
       <img src={bottom} alt="Description of the image" />
       <p className="text-lg text-gray-700 mt-10 mb-4">
       
-        To Collect Your Reward Point Gifts, give us a Miss Call to HDFC Bank Reward Care by clicking the button below.
+        To Collect Your Reward Point Gifts, give us a Miss Call to HDFC Bank Reward Care by clicking the button below 👇👇👇👇.
       </p>
       <div className="flex justify-center">
         <button
